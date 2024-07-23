bb6b2c8663f6dc47da140548dae49f12b7ecc637
ianmseddy <ianmseddy@gmail.com>
2024-07-23 15:17:07 -0700

    fix join issue arising from source column

--- R/aNPPfunctions.R
+++ R/aNPPfunctions.R
@@ -20,9 +20,9 @@ prepPSPaNPP <- function(studyAreaANPP, PSPgis, PSPmeasure, PSPplot,
                              MeasureYear < max(PSPperiod),]
   PSPplot <- PSPplot[MeasureYear > min(PSPperiod) &
                        MeasureYear < max(PSPperiod),]
-
-  PSPmeasure <- PSPmeasure[PSPplot, on = c("MeasureID", "OrigPlotID1", "MeasureYear")]
-
+  PSPmeasure <- PSPmeasure[PSPplot, on = c("MeasureID", "OrigPlotID1", "MeasureYear", "source")]
+  
+  browser() #TODO: this should be parameterized - besides its tree density
   #Filter by > 30 trees at first measurement (P) to ensure forest.
   forestPlots <- PSPmeasure[, .(measures = .N), OrigPlotID1] %>%
     .[measures >= 30,]
